c620d9ff405c7455cc55e215bf4ab1529ee4b4cd
menkagupta <menkag@gmail.com>
2015-09-27 09:40:51 -0700

    added code for assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix: 
+#This function creates a special "matrix" object that can cache its inverse.
+#cacheSolve: 
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#retrieve the inverse from the cache.
+#It also outputs a message if the matrix is non-invertible(singular).
 
-## Write a short comment describing this function
+##makeCacheMatrix()
+###This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <-function(solve) m <<-solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+#cacheSolve: 
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#retrieve the inverse from the cache.
+#It also outputs a message if the matrix is non-invertible(singular).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("Inverse has been already calculated, getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  
+  #Check if the matrix is invertible
+  z<-ifelse(det(data)==0, FALSE, TRUE)
+  
+  if (z){
+   m <- solve(data, ...)
+    x$setinverse(m)
+    m  
+  }
+  else {
+    message("matrix is singular")
+  }
+  
 }
